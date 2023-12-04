0cc978ac4d25e2c1634fb4ea5cddf023e840829c
Elias Arriaga <eliasrosales001@gmail.com>
2023-12-04 06:32:58 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,180 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+var cartCount = 0;
+var productInfo = {};
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1H2VZFrukwJ31PF69PbOJjrZlIqJEmog",
+  authDomain: "n315-ea.firebaseapp.com",
+  projectId: "n315-ea",
+  storageBucket: "n315-ea.appspot.com",
+  messagingSenderId: "244728055229",
+  appId: "1:244728055229:web:c74b21e118bcb575217ed4",
+  measurementId: "G-7SD2L5JQY1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+function initListeners() {
+  $("#createBtn").on("click", (e) => {
+    let fName = $("#fNameC").val();
+    let email = $("#emailC").val();
+    let pw = $("#pwC").val();
+
+    createUserWithEmailAndPassword(auth, email, pw)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert("Error" + errorMessage);
+      });
+  });
+
+  $("#signin").on("click", (e) => {
+    let fName = $("#fName").val();
+    let email = $("#email").val();
+    let pw = $("#pw").val();
+
+    signInWithEmailAndPassword(auth, email, pw)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert("Error Message" + errorMessage);
+      });
+  });
+
+  $("#signOut").on("click", (e) => {
+    signOut(auth).then(() => {
+      console.log("signed out");
+    });
+  });
+}
+
+function changeRoute() {
+  let hashTag = window.location.hash;
+  let pageID = hashTag.replace("#", "");
+  //   console.log(hashTag + ' ' + pageID);
+
+  if (pageID != "" && pageID != "home") {
+    $.get(`pages/${pageID}.html`, function (data) {
+      console.log("data" + data);
+      $("#app").html(data);
+      loadCart();
+    });
+  } else {
+    $.get(`pages/home.html`, function (data) {
+      console.log("data" + data);
+      $("#app").html(data);
+      loadCandy();
+    });
+  }
+}
+
+function loadCart() {
+  $.each(productInfo.Cart, (idx, cartItem) => {
+    let candy = productInfo.Products[cartItem.itemIdx];
+    $(".cart").append(`<div class="candy">
+    <div class="candyImage">
+        <img src="images/${candy.productImage}" alt="">
+    </div>
+    <div class="candyDetails">
+        <h3>${candy.productName} </h3>
+        <p>${candy.productShortDesc} </p>
+        <p class="price">${candy.productPrice}</p>
+        <div id="${idx}" class="buyNow">Delete</div>
+    </div>
+</div>`);
+  });
+}
+
+function loadCandy() {
+  // $(".home").html("");
+  // $.each(productInfo.Products, (idx, candy) => [
+  //   $(".home").append(`<div class="candy">
+  //         <div class="candyImage">
+  //             <img src="images/${candy.productImage}" alt="">
+  //         </div>
+  //         <div class="candyDetails">
+  //             <h3>${candy.productName} </h3>
+  //             <p>${candy.productShortDesc} </p>
+  //             <p class="price">${candy.productPrice}</p>
+  //             <div id="${idx}" class="buyNow">Buy Now</div>
+  //         </div>
+  //     </div>`),
+  // ]);
+
+  $(".buyNow").on("click", (e) => {
+    let productIdx = e.currentTarget.id;
+    let obj = {
+      itemIdx: productIdx,
+    };
+    productInfo.Cart.push(obj);
+    console.log(productInfo.Cart);
+    cartCount = productInfo.Cart.length;
+    updateCartCount();
+  });
+}
+
+function updateCartCount() {
+  if (cartCount == 0) {
+    $(".cartCounter").css("display", "none");
+  } else if (cartCount >= 1) {
+    $(".cartCounter").css("display", "block");
+    $(".cartCounter").html(cartCount);
+  }
+}
+
+function getData() {
+  $.get(`data/data.json`, (data) => {
+    productInfo = data;
+  }).fail(function () {
+    alert("error");
+  });
+}
+
+
+$(document).ready(function() {
+  $('#signin-btn').click(function() {
+      $('#signin-popup').slideToggle(); 
+  });
+
+  $('#signin-form').submit(function(event) {
+      // Add your sign-in logic here
+
+      event.preventDefault();
+
+      $('#signin-popup').slideUp();
+  });
+});
+
+
+
+function initURLListener() {
+  $(window).on("hashchange", changeRoute);
+  changeRoute();
+  updateCartCount();
+  getData();
+
+}
+
+$(document).ready(function () {
+  initListeners();
+  initURLListener();
+});
